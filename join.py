04dacf15cdba75a8fc846a218b2116f47899e5c1
Deepanshu Arora <deepanshu961@gmail.com>
2018-04-15 17:25:20 +0530

    Update join.py

--- join.py
+++ join.py
@@ -3,9 +3,6 @@ import sys
 
 mr = MapReduce.MapReduce()
 
-# =============================
-# Do not modify above this line
-
 def mapper(record):
     # key: order id
     # value: record
@@ -25,8 +22,6 @@ def reducer(key, list_of_values):
           if (v[0] != "order"):
             mr.emit(val+v)
 
-# Do not modify below this line
-# =============================
 if __name__ == '__main__':
   inputdata = open(sys.argv[1])
   mr.execute(inputdata, mapper, reducer)
